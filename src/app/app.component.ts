3ef673477b16364c71a03f31f15a9fe4f09f97a8
patopeth <Plussenhoff@nosis.interno>
2019-01-16 16:57:27 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre = 'Patricio';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  pi = Math.PI;
+
+  a = 0.234;
+
+  salario = 5000.3434;
+
+  heroe = {
+    nombre : 'Logan',
+    clave : 'Wolverine',
+    edad : 500,
+    direccion : {
+      calle : 'Siempre viva',
+      casa : '19'
+    }
+  };
+
+
+  mierdacaca(): any {
+    let caca = new Heroe('asdad', 2);
+
+    return caca;
+  }
+
+}
+
+
+export class Heroe {
+  nombre: string;
+  bio: string;
+  img: string;
+  aparicion: string;
+  casa: string;
+  id?: number;
+
+  constructor(nombre: string, id?: number) {
+    this.id = id;
+    this.nombre = nombre;
+  }
+}
+
+export interface Heroe2 {
+  nombre: string;
+  bio: string;
+  img: string;
+  aparicion: string;
+  casa: string;
+  id?: number;
 }
